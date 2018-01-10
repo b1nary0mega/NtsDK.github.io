7e3a530bccfed3dbb4e3b40008090236b77d5410
Timofey Rechkalov <trechkalov@yandex.ru>
2018-01-10 17:58:44 +0500

    Nanocogs for LARPs.

---
+++ cogs/data.js
@@ -0,0 +1,47 @@
+var visLocales = {
+    ru: {
+        edit: 'Редактировать',
+        del: 'Удалить выбранное',
+        back: 'Назад',
+        addNode: 'Добавить узел',
+        addEdge: 'Добавить ребро',
+        editNode: 'Редактировать узел',
+        editEdge: 'Редактировать ребро',
+        addDescription: 'Кликните в свободное место, чтобы добавить новый узел.',
+        edgeDescription: 'Кликните на узел и протяните ребро к другому узлу, чтобы соединить их.',
+        editEdgeDescription: 'Кликните на контрольные точки и перетащите их к другому узлу, чтобы соединить узлы.',
+        createEdgeError: 'Невозможно соединить ребра в кластер.',
+        deleteClusterError: 'Кластеры не могут быть удалены',
+        editClusterError: 'Кластеры недоступны для редактирования.'
+    },
+    en: {
+        edit: 'Edit',
+        del: 'Delete selected',
+        back: 'Back',
+        addNode: 'Add Node',
+        addEdge: 'Add Edge',
+        editNode: 'Edit Node',
+        editEdge: 'Edit Edge',
+        addDescription: 'Click in an empty space to place a new node.',
+        edgeDescription: 'Click on a node and drag the edge to another node to connect them.',
+        editEdgeDescription: 'Click on the control points and drag them to a node to connect to it.',
+        createEdgeError: 'Cannot link edges to a cluster.',
+        deleteClusterError: 'Clusters cannot be deleted.',
+        editClusterError: 'Clusters cannot be edited.'
+    }
+};
+
+var charExample = `Арагорн	персонаж
+Бильбо	персонаж
+Фродо	персонаж
+кольцо	предмет
+палантир	предмет
+Назгул	персонаж
+Гендальф	персонаж`;
+
+var nodesExample = `Фродо	несет	Кольцо
+Бильбо	ждет	Фродо
+Назгул	ищет	Фродо
+Арагорн	ищет	Назгул
+Гендальф	хранит	палантир
+`;
---
+++ cogs/main.js
@@ -0,0 +1,247 @@
+var nodes = [];
+var edges = [];
+var dictionary = {};
+
+const root = 'body ';
+
+const state = {};
+state.nodesDataset = new vis.DataSet();
+state.edgesDataset = new vis.DataSet();
+
+
+function init(){
+  document.querySelector('.nodesText').value = charExample;
+  document.querySelector('.edgesText').value = nodesExample;
+  
+  document.querySelector('.draw-button').addEventListener('click', draw);
+  document.querySelector('.get-image-button').addEventListener('click', getImage);
+  document.querySelector('.download-button').addEventListener('click', downloadCsv);
+  
+  
+  listen(queryEl(`${root}.save-edge-button`), 'click', updateEdge);
+  listen(queryEl(`${root}.cancel-add-edge-button`), 'click', cancel('.board-add-edge-popup'));
+  
+  draw();
+}
+
+// create a network
+function drawNetwork() {
+  var container = document.getElementById('mynetwork');
+  /*var data = {
+    nodes: nodes,
+    edges: edges
+  };*/
+  var options = {
+    locale: 'ru',
+    locales: visLocales,
+    manipulation: {
+        addNode: function (data, callback) {
+          // filling in the popup DOM elements
+          document.getElementById('operation').innerHTML = "Добавить персонажа";
+          document.getElementById('node-id').value = data.id;
+          document.getElementById('node-label').value = data.label;
+          document.getElementById('node-group').value = "";
+          document.getElementById('saveButton').onclick = saveData.bind(this, data, callback);
+          document.getElementById('cancelButton').onclick = clearPopUp.bind();
+          document.getElementById('network-popUp').style.display = 'block';
+        },
+        editNode: function (data, callback) {
+          // filling in the popup DOM elements
+          document.getElementById('operation').innerHTML = "Редактировать персонажа";
+          document.getElementById('node-id').value = data.id;
+          document.getElementById('node-label').value = data.label;
+          document.getElementById('node-group').value = data.group;
+          document.getElementById('saveButton').onclick = saveData.bind(this, data, callback);
+          document.getElementById('cancelButton').onclick = cancelEdit.bind(this,callback);
+          document.getElementById('network-popUp').style.display = 'block';
+        },
+        addEdge: function (data, callback) {
+          data.arrows ='to';
+          if (data.from == data.to) {
+            var r = confirm("Хотите ли вы присоединить узел к самому себе?");
+            if (r == true) {
+              callback(data);
+            }
+          }
+          else {
+            callback(data);
+          }
+        }
+    }
+  };
+  const data = {
+    nodes: state.nodesDataset,
+    edges: state.edgesDataset
+  };
+  var network = new vis.Network(container, data, options);
+  network.on('selectEdge', showEdgeLabelEditor);
+  network.on('deselectEdge', hideEdgeLabelEditor);
+}
+
+function showEdgeLabelEditor(params) {
+    if (params.edges.length !== 0 && params.nodes.length === 0) {
+        const edge = state.edgesDataset.get(params.edges[0]);
+        state.modifyArgs = {
+            edge,
+            callback(edge2) {
+                if (edge2) {
+                    state.edgesDataset.update(edge2);
+                }
+            },
+            editEdge: true
+        };
+        queryEl(`${root}.add-edge-label-input`).value = edge.label || '';
+        showPopup('.board-add-edge-popup', true);
+    }
+}
+function hideEdgeLabelEditor(params) {
+    showPopup('.board-add-edge-popup', false);
+}
+
+function showPopup(selector, show) {
+    setClassByCondition(queryEl(selector), 'hidden', !show);
+}
+
+function clearPopUp() {
+  document.getElementById('saveButton').onclick = null;
+  document.getElementById('cancelButton').onclick = null;
+  document.getElementById('network-popUp').style.display = 'none';
+}
+
+function cancelEdit(callback) {
+  clearPopUp();
+  callback(null);
+}
+
+function saveData(data,callback) {
+  data.id = document.getElementById('node-id').value;
+  data.label = document.getElementById('node-label').value;
+  data.group = document.getElementById('node-group').value;
+  clearPopUp();
+  callback(data);
+}
+
+function parseData(){
+  nodes = [];
+  const nodesText = document.querySelector('.nodesText').value;
+  nodesText.split('\n').filter(el => el.trim() !== '').forEach((str, index) => {
+    let arr = str.split('\t');
+    dictionary[arr[0].toLowerCase().trim()] = index;
+    nodes.push({
+      id: index,
+      label: arr[0],
+      group: arr[1]
+    })
+  });
+  
+  state.nodesDataset.clear();
+  state.nodesDataset.add(nodes);
+  
+  edges = [];
+  const edgesText = document.querySelector('.edgesText').value;
+  edgesText.split('\n').filter(el => el.trim() !== '').forEach((str, index) => {
+    let arr = str.split('\t');
+    
+    let from = arr[0].toLowerCase().trim();
+    let to = arr[2].toLowerCase().trim();
+    if(dictionary[from] !== undefined && dictionary[to] !== undefined){
+      edges.push({
+        from: dictionary[from],
+        to: dictionary[to],
+        label: arr[1],
+        arrows:'to',
+      })
+    }
+    
+  });
+  
+  state.edgesDataset.clear();
+  state.edgesDataset.add(edges);
+}
+
+function draw(){
+  parseData();
+  drawNetwork();
+}
+
+function getImage(event){
+  var canvas = document.querySelector("canvas");
+  
+  var context = canvas.getContext("2d");
+  var w = canvas.width;
+  var h = canvas.height;
+  
+  context.globalCompositeOperation = "destination-over";
+  context.fillStyle = "#ffffff";
+  context.fillRect(0,0,w,h);
+  
+  //canvas.globalAlpha = 0.5;
+  var img    = canvas.toDataURL("image/png");
+  var link = document.querySelector(".link");
+  event.target.href = img;
+  draw();
+  //document.write('<img src="'+img+'"/>');
+}
+
+function updateEdge() {
+    const input = queryEl(`${root}.add-edge-label-input`);
+    const label = input.value.trim();
+    const { edge } = state.modifyArgs;
+    /*DBMS.setEdgeLabel(edge.from, edge.to, label, (err) => {
+        if (err) { Utils.handleError(err); return; }*/
+
+        edge.label = label;
+        showPopup('.board-add-edge-popup', false);
+        input.value = '';
+        state.modifyArgs.callback(edge);
+   // });
+}
+
+function cancel(selector) {
+    return () => {
+        showPopup(selector, false);
+        state.modifyArgs.callback();
+    };
+}
+
+function downloadCsv() {
+    /*const el = queryEl(`${root}.profile-item-selector`);
+    const selected = [];
+    for (let i = 0; i < el.options.length; i += 1) {
+        selected[i] = el.options[i].selected;
+    }
+
+    const dataArrays = makePrintData();
+    const cleanArrays = dataArrays.map(dataArray => dataArray.filter((item, index) => selected[index]).map(R.prop('value')));*/
+
+    //FileUtils.arr2d2Csv(cleanArrays, 'table.csv');
+    
+    var arr = state.nodesDataset.map((node) => [node.label, node.group]);
+    var arr2 = state.edgesDataset.map((edge) => [state.nodesDataset.get(edge.from).label, edge.label, 
+      state.nodesDataset.get(edge.to).label]);
+    //state.edgesDataset = new vis.DataSet();
+
+    arr2d2Csv(arr.concat(arr2), 'cogs.csv');
+}
+
+function preprocessCsvStr(str) {
+    if (!(typeof str === 'string' || str instanceof String)) {
+        return str;
+    }
+    let result = str.replace(/"/g, '""');
+    if (result.search(/("|,|\n)/g) >= 0) {
+        result = `"${result}"`;
+    }
+    return result;
+}
+
+var arr2d2Csv = (arr, fileName) => {
+    const csv = `\ufeff${arr.map(dataArray => dataArray.map(preprocessCsvStr).join(';')).join('\n')}`;
+
+    const out = new Blob([csv], {
+        type: 'text/csv;charset=utf-8;'
+    });
+    saveAs(out, fileName);
+};
+
+init();
